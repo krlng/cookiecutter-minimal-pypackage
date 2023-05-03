1c854bcab3195181d0172bd342e83ecb507784df
Nico Kreiling <nicokreiling@Nicos-MBP-2.localdomain>
2023-05-03 13:52:22 +0200

    update to poetry

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,5 +1,10 @@
 """{{ cookiecutter.package_name }} - {{ cookiecutter.package_description }}"""
 
+from pathlib import Path
+
 __version__ = '{{ cookiecutter.package_version }}'
 __author__ = '{{ cookiecutter.author_name }} <{{ cookiecutter.author_email }}>'
 __all__ = []
+
+PROJECT_DIR = Path(__file__).parents[1]
+DATA_DIR = PROJECT_DIR / "data"
